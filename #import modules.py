7e18199050001a82589572e548bcbd142f371da8
PPYOL1 <ppyol1@nottingham.ac.uk>
2023-06-19 16:34:45 +0100

    Accelerometer code files created

---
+++ #import modules.py
@@ -0,0 +1,42 @@
+#import modules
+import serial
+import time
+import matplotlib.pyplot as plt
+
+ser = serial.Serial('COM8', 9600, timeout=None) #reading data from port "COM8"
+time.sleep(0.05)
+
+#empty arrays for data storage
+data_pkz = []
+data_avgz = []
+
+fig = plt.figure()
+plt.ylim(-2,2)
+
+
+for i in range(25):
+    #reading a byte
+    line = ser.readline()
+    if line:
+        string = line.decode() #convert byte data to unicode string
+        string = string.replace(" ","") #remove all spaces from string
+        pk_z = float(string[-7:])   
+        avg_z = float(string[40:45])
+
+        print("z-accleration: ", pk_z)
+        #print("avg z-accleration: ", avg_z)
+
+        data_pkz.append(pk_z) #append values to array "data_pkz and data_avgz"
+        data_avgz.append(avg_z)
+        
+        plt.clf()
+        plt.xlabel('Time')
+        plt.ylabel('z-acceleration (g)')
+        plt.plot(data_pkz,'.-')
+        plt.pause(0.01)
+
+ser.close()
+
+plt.show()
+
+
---
+++ string_slicing_test.py
@@ -0,0 +1,46 @@
+#import modules
+import serial
+import time
+import matplotlib.pyplot as plt
+import re
+
+ser = serial.Serial('COM8', 19200, timeout=None) #reading data from port "COM8"
+time.sleep(0.01)
+
+#empty arrays for data storage
+data_pkz = []
+data_avgz = []
+
+for i in range(5):
+    #reading a byte
+    line = ser.readline()
+    if line:
+        string = line.decode() #convert byte data to unicode string
+        string = string.replace(" ","") #remove all spaces from string
+        string = re.IGNORECASE('string')
+        print(string)
+        #pk_z = float(string[-7:])   
+        #avg_z = float(string[40:45])
+
+        #print("z-accleration: ", pk_z)
+        #print("avg z-accleration: ", avg_z)
+
+        #data_pkz.append(pk_z) #append values to array "data_pkz and data_avgz"
+        #data_avgz.append(avg_z)
+
+ser.close()
+
+print(string)
+# %%
+#building plot
+fig, (ax1,ax2) = plt.subplots(2)
+
+ax1.plot(data_pkz, '.-')
+ax1.set_ylabel('z-acceleration (g)')
+
+ax2.plot(data_avgz, '.-')
+ax2.set_xlabel('Time')
+ax2.set_ylabel('Avg z-acceleration (g)')
+ax1.grid()
+ax2.grid()
+plt.show()
